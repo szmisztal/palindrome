a6c21b8263e5abf490bc01b0ab6c5ee41a420179
Szymon Misztal <sz.misztal@gmail.com>
2023-02-22 13:15:37 +0100

    change palindrome.py code

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,7 @@
-string = "kajak"
+string = "potop"
 
 def is_string_palindrome(string):
   return string == string[::-1]
 
-print(bool(is_string_palindrome))
+answer = is_string_palindrome(string)
+print(bool(answer))
